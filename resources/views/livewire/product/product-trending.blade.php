c09af6942112570195149226246651a413d262d0
Karamoko Coulibaly <k.coulibaly@ecole-ipssi.net>
2021-09-16 16:13:46 +0200

    Mise à jour de la page des dernières commande de l'utilisateur fix lorsque celui ci n'a pas de commande.

--- resources/views/livewire/product/product-trending.blade.php
+++ resources/views/livewire/product/product-trending.blade.php
@@ -4,13 +4,6 @@
         <div class="grid-body">
             <div class="item-wrapper">
                 <div class="table-responsive">
-                    {{-- <div class="pt-2 relative mx-auto text-gray-600 flex flex-row-reverse mr-6 mb-2">
-                        <input class="border-2 border-gray-300 bg-white h-10 px-5 pr-16 rounded-lg text-sm focus:outline-none"
-                            type="search"
-                            name="search"
-                            placeholder="Rechercher"
-                            wire:model="search">
-                    </div> --}}
                     <table class="table info-table table-striped">
                         <thead>
                             <tr>
--- resources/views/livewire/shop/shop-orders.blade.php
+++ resources/views/livewire/shop/shop-orders.blade.php
@@ -93,59 +93,72 @@
             </div>
         @endforeach
     <!-- Billing -->
-    <div class="mt-16">
-        <h2 class="sr-only">Résumé de la facture</h2>
-    
-        <div class="bg-gray-100 py-6 px-4 sm:px-6 sm:rounded-lg lg:px-8 lg:py-8 lg:grid lg:grid-cols-12 lg:gap-x-8">
-            <dl class="grid grid-cols-2 gap-6 text-sm sm:grid-cols-2 md:gap-x-8 lg:col-span-7">
-            <div>
-                <dt class="font-medium text-gray-900">Adresse de Facturation</dt>
-                <dd class="mt-3 text-gray-500">
-                    <span class="block">{{$address->name}}</span>
-                    <span class="block">{{$address->street}}</span>
-                    <span class="block">{{ $address->country == '' ? 'France': $address->country}}, {{$address->city =='' ? "Paris" : $address->city }}, {{$address->postal_code == "" ? "75001" : $address->postal_code}}</span>
-                </dd>
-            </div>
-            <div>
-                <dt class="font-medium text-gray-900">Informations de paiements</dt>
-                <div class="mt-3">
-                <dd class="-ml-4 -mt-4 flex flex-wrap">
-                    <div class="ml-4 mt-4 flex-shrink-0">
-                    <svg aria-hidden="true" width="36" height="24" viewBox="0 0 36 24" xmlns="http://www.w3.org/2000/svg" class="h-6 w-auto">
-                        <rect width="36" height="24" rx="4" fill="#224DBA" />
-                        <path d="M10.925 15.673H8.874l-1.538-6c-.073-.276-.228-.52-.456-.635A6.575 6.575 0 005 8.403v-.231h3.304c.456 0 .798.347.855.75l.798 4.328 2.05-5.078h1.994l-3.076 7.5zm4.216 0h-1.937L14.8 8.172h1.937l-1.595 7.5zm4.101-5.422c.057-.404.399-.635.798-.635a3.54 3.54 0 011.88.346l.342-1.615A4.808 4.808 0 0020.496 8c-1.88 0-3.248 1.039-3.248 2.481 0 1.097.969 1.673 1.653 2.02.74.346 1.025.577.968.923 0 .519-.57.75-1.139.75a4.795 4.795 0 01-1.994-.462l-.342 1.616a5.48 5.48 0 002.108.404c2.108.057 3.418-.981 3.418-2.539 0-1.962-2.678-2.077-2.678-2.942zm9.457 5.422L27.16 8.172h-1.652a.858.858 0 00-.798.577l-2.848 6.924h1.994l.398-1.096h2.45l.228 1.096h1.766zm-2.905-5.482l.57 2.827h-1.596l1.026-2.827z" fill="#fff" />
-                    </svg>
-                    <p class="sr-only">Visa</p>
-                    </div>
-                    <div class="ml-4 mt-4">
-                    <p class="text-gray-900">
-                        Fin 4242
-                    </p>
-                    <p class="text-gray-600">
-                        Expire 02 / 24
-                    </p>
-                    </div>
-                </dd>
+    @if(count($ordersList) > 0)
+        <div class="mt-16">
+            <h2 class="sr-only">Résumé de la facture</h2>
+        
+            <div class="bg-gray-100 py-6 px-4 sm:px-6 sm:rounded-lg lg:px-8 lg:py-8 lg:grid lg:grid-cols-12 lg:gap-x-8">
+                <dl class="grid grid-cols-2 gap-6 text-sm sm:grid-cols-2 md:gap-x-8 lg:col-span-7">
+                <div>
+                    <dt class="font-medium text-gray-900">Adresse de Facturation</dt>
+                    <dd class="mt-3 text-gray-500">
+                        <span class="block">{{$address->name}}</span>
+                        <span class="block">{{$address->street}}</span>
+                        <span class="block">{{ $address->country == '' ? 'France': $address->country}}, {{$address->city =='' ? "Paris" : $address->city }}, {{$address->postal_code == "" ? "75001" : $address->postal_code}}</span>
+                    </dd>
                 </div>
+                <div>
+                    <dt class="font-medium text-gray-900">Informations de paiements</dt>
+                    <div class="mt-3">
+                    <dd class="-ml-4 -mt-4 flex flex-wrap">
+                        <div class="ml-4 mt-4 flex-shrink-0">
+                        <svg aria-hidden="true" width="36" height="24" viewBox="0 0 36 24" xmlns="http://www.w3.org/2000/svg" class="h-6 w-auto">
+                            <rect width="36" height="24" rx="4" fill="#224DBA" />
+                            <path d="M10.925 15.673H8.874l-1.538-6c-.073-.276-.228-.52-.456-.635A6.575 6.575 0 005 8.403v-.231h3.304c.456 0 .798.347.855.75l.798 4.328 2.05-5.078h1.994l-3.076 7.5zm4.216 0h-1.937L14.8 8.172h1.937l-1.595 7.5zm4.101-5.422c.057-.404.399-.635.798-.635a3.54 3.54 0 011.88.346l.342-1.615A4.808 4.808 0 0020.496 8c-1.88 0-3.248 1.039-3.248 2.481 0 1.097.969 1.673 1.653 2.02.74.346 1.025.577.968.923 0 .519-.57.75-1.139.75a4.795 4.795 0 01-1.994-.462l-.342 1.616a5.48 5.48 0 002.108.404c2.108.057 3.418-.981 3.418-2.539 0-1.962-2.678-2.077-2.678-2.942zm9.457 5.422L27.16 8.172h-1.652a.858.858 0 00-.798.577l-2.848 6.924h1.994l.398-1.096h2.45l.228 1.096h1.766zm-2.905-5.482l.57 2.827h-1.596l1.026-2.827z" fill="#fff" />
+                        </svg>
+                        <p class="sr-only">Visa</p>
+                        </div>
+                        <div class="ml-4 mt-4">
+                        <p class="text-gray-900">
+                            Fin 4242
+                        </p>
+                        <p class="text-gray-600">
+                            Expire 02 / 24
+                        </p>
+                        </div>
+                    </dd>
+                    </div>
+                </div>
+                </dl>
+        
+                <dl class="mt-8 divide-y divide-gray-200 text-sm lg:mt-0 lg:col-span-5">
+                    Les prix inclus la TVA de 20%.
+                <div class="pb-4 flex items-center justify-between">
+                    <dt class="text-gray-600">{{__('Subtotal')}}</dt>
+                    <dd class="font-medium text-gray-900">{{($order->cart->total)-5}}.00 €</dd>
+                </div>
+                <div class="py-4 flex items-center justify-between">
+                    <dt class="text-gray-600">Livraison</dt>
+                    <dd class="font-medium text-gray-900">5.OO €</dd>
+                </div>
+                <div class="pt-4 flex items-center justify-between">
+                    <dt class="font-medium text-gray-900">Total de la commande</dt>
+                    <dd class="font-medium text-indigo-600">{{$order->cart->total}}.00 €</dd>
+                </div>
+                </dl>
             </div>
-            </dl>
-    
-            <dl class="mt-8 divide-y divide-gray-200 text-sm lg:mt-0 lg:col-span-5">
-                Les prix inclus la TVA de 20%.
-            <div class="pb-4 flex items-center justify-between">
-                <dt class="text-gray-600">{{__('Subtotal')}}</dt>
-                <dd class="font-medium text-gray-900">{{($order->cart->total)-5}}.00 €</dd>
-            </div>
-            <div class="py-4 flex items-center justify-between">
-                <dt class="text-gray-600">Livraison</dt>
-                <dd class="font-medium text-gray-900">5.OO €</dd>
-            </div>
-            <div class="pt-4 flex items-center justify-between">
-                <dt class="font-medium text-gray-900">Total de la commande</dt>
-                <dd class="font-medium text-indigo-600">{{$order->cart->total}}.00 €</dd>
-            </div>
-            </dl>
         </div>
-    </div>
+
+    @else 
+        <div class="grid">
+            <p>
+                Vous n'avez pas encore de commande ...
+                
+            </p>
+            <p>
+                On vous attend <a class="w-full bg-indigo-600 border border-transparent rounded-md shadow-sm py-2 px-4 flex items-center justify-center text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{route('shop.products')}}">ICI</a>
+            </p>
+        </div>
+    @endif
     {{$ordersList->links()}}
 </div>
